2d2abff950275a46006ac69efcc8de2884e1df40
Alexis Gousseau <alexis.gousseau@ldc.fr>
2022-07-25 12:08:17 +0200

    fix front details

--- js/main_carte.php
+++ js/main_carte.php
@@ -2,18 +2,18 @@
     window.addEventListener("load", function(event) {
         var loader = document.querySelector(".loader")
         loader.style.opacity = "0"
-        setTimeout(function(){
+        setTimeout(function() {
             loader.style.display = "none"
         }, 1000)
 
         var btnExit = document.querySelector(".btnExit")
-        btnExit.addEventListener("click", function(){
+        btnExit.addEventListener("click", function() {
             loader.style.display = "flex"
-            setTimeout(function(){
+            setTimeout(function() {
                 loader.style.transition = "1s"
                 loader.style.opacity = "1"
             }, 100)
-            setTimeout(function(){
+            setTimeout(function() {
                 window.location.href = "index.php"
             }, 1000)
         })
@@ -66,7 +66,7 @@
         });
 
         //Attribution de ces cartes dans un menu
-        var centre = [46, 1]
+        var centre = [46, 2]
         var zoom = 6
 
         var map = L.map('map', {
@@ -101,20 +101,20 @@
         var xmlhttp = new XMLHttpRequest()
         var url = ""
 
-        <?php 
-        if($_POST['lang'] == 'fr'){
+        <?php
+        if ($_POST['lang'] == 'fr') {
         ?>
             url = "json/mainFr.json"
         <?php
-        }else if($_POST['lang'] == 'en'){
+        } else if ($_POST['lang'] == 'en') {
         ?>
             url = "json/mainEn.json"
         <?php
         }
         ?>
 
-        xmlhttp.onreadystatechange = function(){
-            if (this.readyState == 4 && this.status == 200){
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
                 var tabJson = JSON.parse(this.responseText)
                 myFunction(tabJson)
             }
@@ -122,56 +122,55 @@
         xmlhttp.open("GET", url, true)
         xmlhttp.send()
 
-        function myFunction(tab){
+        function myFunction(tab) {
             //Mise en place de certaines variables qui doivent être réinitialisées quand on réappel le fichier .json
             var res = ""
             pays = []
             var j = 0
             var count = 0
-            
+
             //Parcours le json
-            for(let i = 0 ; i < tab.length ; i ++){
+            for (let i = 0; i < tab.length; i++) {
                 countDestinations = i + 1
                 document.querySelector(".installationsTitre").innerHTML = "<?php echo MAP_DESTINATIONS; ?>(" + countDestinations + ")"
                 //Création des markers
-                if(tab[i].pays === "France"){
+                if (tab[i].pays === "France") {
                     var marker = L.marker([tab[i].latitude, tab[i].longitude], {
                         icon: L.icon({
                             iconUrl: "data/pictos/marqueur_bleu.png",
                             shadowUrl: '',
-                            iconSize:     [26, 40], // size of the icon
-                            shadowSize:   [0, 0],   // size of the shadow
-                            iconAnchor:   [13, 40], // point of the icon which will correspond to marker's location
-                            shadowAnchor: [4, 62],  // the same for the shadow
-                            popupAnchor:  [0, -65] // point from which the popup should open relative to the iconAnchor
+                            iconSize: [26, 40], // size of the icon
+                            shadowSize: [0, 0], // size of the shadow
+                            iconAnchor: [13, 40], // point of the icon which will correspond to marker's location
+                            shadowAnchor: [4, 62], // the same for the shadow
+                            popupAnchor: [0, -65] // point from which the popup should open relative to the iconAnchor
                         })
                     })
                 }
-                if(tab[i].pays === "Espagne" || tab[i].pays === "Spain"){
+                if (tab[i].pays === "Espagne" || tab[i].pays === "Spain") {
                     var marker = L.marker([tab[i].latitude, tab[i].longitude], {
                         icon: L.icon({
                             iconUrl: "data/pictos/marqueur_orange.png",
                             shadowUrl: '',
-                            iconSize:     [26, 40], // size of the icon
-                            shadowSize:   [0, 0],   // size of the shadow
-                            iconAnchor:   [13, 40], // point of the icon which will correspond to marker's location
-                            shadowAnchor: [4, 62],  // the same for the shadow
-                            popupAnchor:  [0, -65] // point from which the popup should open relative to the iconAnchor
+                            iconSize: [26, 40], // size of the icon
+                            shadowSize: [0, 0], // size of the shadow
+                            iconAnchor: [13, 40], // point of the icon which will correspond to marker's location
+                            shadowAnchor: [4, 62], // the same for the shadow
+                            popupAnchor: [0, -65] // point from which the popup should open relative to the iconAnchor
                         })
                     })
                 }
-                marker.addEventListener("click", function(){
+                marker.addEventListener("click", function() {
                     var tables = document.querySelectorAll("table")
                     tableInitial()
                     tables[i].style.backgroundColor = "var(--color2)"
-                    setTimeout(function(){
-                        if(document.querySelector(".leaflet-popup-close-button") != null){
+                    setTimeout(function() {
+                        if (document.querySelector(".leaflet-popup-close-button") != null) {
                             var leafletPopupCloseButton = document.querySelector(".leaflet-popup-close-button")
-                            leafletPopupCloseButton.addEventListener("click", function(){
+                            leafletPopupCloseButton.addEventListener("click", function() {
                                 tableInitial()
                             })
-                        }
-                        else{
+                        } else {
                             tableInitial()
                         }
                     }, 300)
@@ -179,30 +178,29 @@
 
                 //Affiche TOUTES les installations et leurs caractéristiques
                 //Ajout du contrat à un tableau SEULEMENT s'il n'existe pas dans ce même tableau
-                for (let p = 0 ; p < pays.length ; p ++) {
-                    if(pays[p] != tab[i].pays){
+                for (let p = 0; p < pays.length; p++) {
+                    if (pays[p] != tab[i].pays) {
                         count = count + 1
                     }
                 }
-                if(count === pays.length){
+                if (count === pays.length) {
                     pays.push(tab[i].pays)
                     markers.push(L.markerClusterGroup())
                 }
                 //Remise à zéro
                 count = 0
 
-                if(j === 0){
+                if (j === 0) {
                     res += "<table><tbody><tr class='installationsContentGauche'><td>" + tab[i].imgDesc + "</td>" +
-                    "<td class='installationsContentDroite'>" + tab[i].annee + "</td></tbody></table>"
-                }
-                else{
+                        "<td class='installationsContentDroite'>" + tab[i].annee + "</td></tbody></table>"
+                } else {
                     res += "<hr><table><tbody><tr class='installationsContentGauche'><td>" + tab[i].imgDesc + "</td>" +
-                    "<td class='installationsContentDroite'>" + tab[i].annee + "</td></tbody></table>"
+                        "<td class='installationsContentDroite'>" + tab[i].annee + "</td></tbody></table>"
                 }
-                
+
                 //Récupération de toutes les informations concernant les installations
-                resTab.push("<center><b><h3>" + tab[i].annee + "</h3><b><hr>" + tab[i].pays + ", " + tab[i].ville + "<br>" + 
-                "<br><a href='" + tab[i].lien + "' target='_blank'><img class='popupImg' src='" + tab[i].img + "'></a><br><em>" + tab[i].imgDesc + "</em>")
+                resTab.push("<center><b><h3>" + tab[i].annee + "</h3><b><hr>" + tab[i].pays + ", " + tab[i].ville + "<br>" +
+                    "<br><a href='" + tab[i].lien + "' target='_blank'><img class='popupImg' src='" + tab[i].img + "'></a><br><em>" + tab[i].imgDesc + "</em>")
 
                 //Ajout des latitudes et longitudes dans un tableau
                 lat.push(tab[i].latitude)
@@ -215,15 +213,15 @@
                 j = j + 1
 
                 //Ajout des marqueurs au groupement de marqueurs respectif
-                for (let m = 0 ; m < pays.length ; m ++) {
-                    if(tab[i].pays === pays[m]){
+                for (let m = 0; m < pays.length; m++) {
+                    if (tab[i].pays === pays[m]) {
                         markers[m].addLayer(marker)
                     }
                 }
             }
 
             //Ajout du groupement de marqueurs à la carte
-            for (let i = 0 ; i < markers.length ; i ++) {
+            for (let i = 0; i < markers.length; i++) {
                 map.addLayer(markers[i]);
             }
 
@@ -232,44 +230,44 @@
 
             //Modification de la vue lorsqu'un clique sur une installation a lieu
             var test = document.querySelectorAll("table")
-            for (let t = 0 ; t < test.length ; t ++) {
-                test[t].addEventListener("click", function(){
+            for (let t = 0; t < test.length; t++) {
+                test[t].addEventListener("click", function() {
                     var tables = document.querySelectorAll("table")
                     map.setView([lat[t], lng[t]], 12)
                     tableInitial()
                     tables[t].style.backgroundColor = "var(--color2)"
                     markerpopup[t].openPopup()
-                    setTimeout(function(){
+                    setTimeout(function() {
                         var leafletPopupCloseButton = document.querySelector(".leaflet-popup-close-button")
-                        leafletPopupCloseButton.addEventListener("click", function(){
+                        leafletPopupCloseButton.addEventListener("click", function() {
                             tableInitial()
                         })
                     }, 300)
                 })
-            } 
+            }
         }
 
         //Quand on clique sur la flèche pour revenir aux différents contrats
         var flecheRetourContrats = document.querySelector(".installationsRetourContrats")
 
-        function tableInitial(){
+        function tableInitial() {
             map.addEventListener("click", tableInitial)
             var tables = document.querySelectorAll("table")
-            for (let m = 0 ; m < tables.length ; m ++) {
+            for (let m = 0; m < tables.length; m++) {
                 tables[m].style.background = "none"
             }
         }
 
         //Quand on clique sur le bouton recentrer
         var btnRecentrer = document.querySelector(".btnRecentrer")
-        btnRecentrer.addEventListener("click", function(){
+        btnRecentrer.addEventListener("click", function() {
             closePopup()
             map.setView(centre, zoom)
             tableInitial()
         })
 
         //Fermer tous les popups
-        function closePopup(){
+        function closePopup() {
             map.closePopup()
         }
 
@@ -277,10 +275,10 @@
         let btnPleinEcran = document.querySelector(".btnPleinEcran")
         let btnSVGon = document.querySelector(".btnSVGon")
         let btnSVGoff = document.querySelector(".btnSVGoff")
-        btnPleinEcran.addEventListener("click", function(){
+        btnPleinEcran.addEventListener("click", function() {
             document.querySelector("body").requestFullscreen()
             btnSVGoff.classList.toggle("displayNone")
-            if(btnSVGon.classList.toggle("displayNone") === true){
+            if (btnSVGon.classList.toggle("displayNone") === true) {
                 document.exitFullscreen()
             }
         })
@@ -292,26 +290,23 @@
         let parametres = document.querySelector(".parametres")
         let parametresBtnSVGcross = document.querySelector(".parametresBtnSVGcross")
         let btnSVGparametres = document.querySelector(".btnSVGparametres")
-        btnSVGFleche.addEventListener("click", function(){
-            if(parametres.classList.contains("displayNone") && parametresBtnSVGcross.classList.contains("displayNone")){
-                if(menu.classList.contains("menuActivated")){
+        btnSVGFleche.addEventListener("click", function() {
+            if (parametres.classList.contains("displayNone") && parametresBtnSVGcross.classList.contains("displayNone")) {
+                if (menu.classList.contains("menuActivated")) {
                     menuContentInstallations.classList.toggle("displayNone")
                     menu.classList.toggle("menuActivated")
-                }
-                else{
+                } else {
                     menu.classList.toggle("menuActivated")
-                    setTimeout(function(){
+                    setTimeout(function() {
                         menuContentInstallations.classList.toggle("displayNone")
                     }, 300)
                 }
-            }
-            else if(menu.classList.contains("menuActivated")){
+            } else if (menu.classList.contains("menuActivated")) {
                 parametres.classList.toggle("displayNone")
                 menu.classList.toggle("menuActivated")
-            }
-            else{
+            } else {
                 menu.classList.toggle("menuActivated")
-                setTimeout(function(){
+                setTimeout(function() {
                     parametres.classList.toggle("displayNone")
                 }, 300)
             }
@@ -321,8 +316,8 @@
         btnSVGparametres.addEventListener("click", handler__parametre)
         parametresBtnSVGcross.addEventListener("click", handler__parametre)
 
-        function handler__parametre(){
-            if(menu.classList.contains("menuActivated") === false){
+        function handler__parametre() {
+            if (menu.classList.contains("menuActivated") === false) {
                 menuContentInstallations.classList.toggle("displayNone")
                 parametres.classList.toggle("displayNone")
                 parametresBtnSVGcross.classList.toggle("displayNone")
@@ -330,16 +325,15 @@
             }
         }
 
-        document.querySelector(".switch__checkBox").addEventListener("click", function(){
+        document.querySelector(".switch__checkBox").addEventListener("click", function() {
             let test = document.querySelector(".switch__checkBox")
-            if(test.getAttribute("checked") === null || test.getAttribute("checked") === "false"){
+            if (test.getAttribute("checked") === null || test.getAttribute("checked") === "false") {
                 document.documentElement.style.setProperty('--color1', '#0f1823');
                 document.documentElement.style.setProperty('--color2', '#3E3E3E');
                 document.documentElement.style.setProperty('--color5', '#FFFFFF');
                 test.setAttribute("checked", "true")
                 document.cookie = "darkMode=true; expires=Thu, 18 Dec 2100 12:00:00 UTC; path=/";
-            }
-            else if(test.getAttribute("checked") != null || test.getAttribute("checked") === "true"){
+            } else if (test.getAttribute("checked") != null || test.getAttribute("checked") === "true") {
                 document.documentElement.style.setProperty('--color1', '#FFFFFF');
                 document.documentElement.style.setProperty('--color2', '#E5E5E5');
                 document.documentElement.style.setProperty('--color5', '#2E2E2E');
@@ -363,17 +357,17 @@
         document.querySelector(".leaflet-control-zoom-out").title = "<?php echo MAP_TITLE_ZOOM_OUT; ?>"
         document.querySelector(".leaflet-control-zoom-out").style.backgroundColor = "var(--color1)"
 
-        document.querySelector(".leaflet-control-layers-toggle").parentNode.style.backgroundColor = "var(--color1)"
+        document.querySelector(".leaflet-control-layers-toggle").style.backgroundColor = "var(--color1)"
 
         <?php
-            if(isset($_COOKIE["darkMode"]) && $_COOKIE["darkMode"] === "true"){
-                ?>
-                document.querySelector(".switch__checkBox").setAttribute("checked", "true")
-                document.documentElement.style.setProperty('--color1', '#0f1823');
-                document.documentElement.style.setProperty('--color2', '#3E3E3E');
-                document.documentElement.style.setProperty('--color5', '#FFFFFF');
-                <?php 
-            }
+        if (isset($_COOKIE["darkMode"]) && $_COOKIE["darkMode"] === "true") {
+        ?>
+            document.querySelector(".switch__checkBox").setAttribute("checked", "true")
+            document.documentElement.style.setProperty('--color1', '#0f1823');
+            document.documentElement.style.setProperty('--color2', '#3E3E3E');
+            document.documentElement.style.setProperty('--color5', '#FFFFFF');
+        <?php
+        }
         ?>
     })
 </script>
